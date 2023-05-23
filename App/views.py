6af2dc60b2e798ad92110f9fc1637257865fdfec
saihein <saiheinthetaung669@gmail.com>
2023-05-23 11:33:59 +0630

    second push

--- App/views.py
+++ App/views.py
@@ -9,7 +9,10 @@ import re
 def home(request):
     return render(request, 'home.html')
 
-data={}
+
+data = {}
+
+
 def extract_text(request):
     if request.method == 'POST':
         image_file = request.FILES['image']
@@ -17,8 +20,17 @@ def extract_text(request):
 
         image = Image.open(image_file)
 
-        texts = pytesseract.image_to_string(image)
-
-        return render(request, 'extract.html', {'text': texts, 'image': image_file})
-    return render(request, 'extract.html')
+        res = pytesseract.image_to_string(image)
+        result = res.split('\n')
+        for text in result:
+            if re.findall('^Ma|^Maung|^Daw|^U', text):
+                data['Name'] = text[0:len(text) - 11]
+            if re.findall('^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)', text):
+                data['Date'] = text
+            if re.findall('Ks$', text):
+                data['Amount'] = text[1:]
+            if re.findall('^\d{10}', text):
+                data['Id'] = text
+        return render(request, 'extract.html', {'data': data})
+    return render(request, 'home.html')
 # Create your views here.
